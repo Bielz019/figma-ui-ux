74dd1f88efa1affbffba620b8b48a239511f8f09
Campos <145881098+Bielz019@users.noreply.github.com>
2025-10-31 11:47:03 -0300

    Add files via upload

--- src/Cadastro.jsx
+++ src/Cadastro.jsx
@@ -1,30 +1,29 @@
-import React from 'react';
-import './App.css';
-import { Link } from "react-router";
+import React from "react";
+import "./Cadastro.css";
+import { Link } from "react-router-dom";
+
 function Cadastro() {
   return (
-    <div className="login-container">
-      <div className="login-box">
-        <h1 className="login-title">Cadastro</h1>
+    <div className="cadastro-container">
+      <div className="cadastro-box">
+        <h1 className="cadastro-title">Cadastro</h1>
 
-        <label className="login-label">Usuario:</label>
-        <input type="text" className="login-input" />
+        <label className="cadastro-label">Nome:</label>
+        <input type="text" className="cadastro-input" placeholder="Digite seu nome completo" />
 
-        <label className="login-label">Senha:</label>
-        <input type="password" className="login-input" />
+        <label className="cadastro-label">E-mail:</label>
+        <input type="email" className="cadastro-input" placeholder="Digite seu e-mail" />
 
-        <div className="options-row">
-          <label className="checkbox-container">
-            <input type="checkbox" />
-            <span>Salvar Cadastro</span>
-          </label>
-          <a href="#" className="forgot-password">Esqueceu a senha?</a>
-        </div>
+        <label className="cadastro-label">Senha:</label>
+        <input type="password" className="cadastro-input" placeholder="Crie uma senha" />
 
-        <button className="login-button">CADASTRAR</button>
+        <label className="cadastro-label">Confirmar Senha:</label>
+        <input type="password" className="cadastro-input" placeholder="Confirme sua senha" />
+
+        <button className="cadastro-button">CADASTRAR</button>
 
         <div className="social-login">
-          <p className="social-text">Logar com</p>
+          <p className="social-text">Ou cadastre-se com</p>
           <div className="icons">
             <img src="https://upload.wikimedia.org/wikipedia/commons/thumb/c/c1/Google_%22G%22_logo.svg/2048px-Google_%22G%22_logo.svg.png" alt="Google" />
             <img src="https://upload.wikimedia.org/wikipedia/commons/9/91/Octicons-mark-github.svg" alt="Github" />
